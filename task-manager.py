70d030694c7446cc73a76c8d027771f60ae50894
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-09 20:19:09 -0500

    feature/input field and button to add tasks

--- task-manager.py
+++ task-manager.py
@@ -21,6 +21,9 @@ def main():
     # Create the task frame and store it in the 'task-frame' variable.
     task_frame = create_task_frame(root)
 
+    # Create the nput field and add task button.
+    create_input_and_add_button(root, task_frame)
+
 # Execute the main function when the script is run.
 if __name__ == "__main__":
     main()
@@ -32,3 +35,18 @@ def create_task_frame(root):
     task_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
     # Return the created frame.
     return task_frame
+
+# Create an iniput field and button to add tasks.
+
+def create_input_and_add_button(root, task_frame):
+    # Create an entry widget for user to input tasks.
+    task_entry = ttk.Entry(task_frame, width=20)
+    # Place the task_entry widget in the task_frame usinig the grid geometry manager.
+    task_entry.grid(row=0, column=0, padx=5, pady=5)
+
+    # Create a Button widget to add tasks.
+    add_task_button = ttk.Button(task_frame, text="Add task", command=lambda: add_task(root, task_entry))
+    # Place the add_task_button widget in the task_frame using the grid geometry manager.
+    add_task_button.grid(row=0, column=1, padx=5, pady=5)
+
+    # Call this funciton in the main().
